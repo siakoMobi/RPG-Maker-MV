f84cbaaaba48c755b74258180bb1b3d34e56e3f6
siakochen <siakochen@gmail.com>
2016-08-31 15:20:12 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RPG-Maker-MV
---
+++ v1.3.1/rpg_core/CacheEntry.js
@@ -0,0 +1,151 @@
+//-----------------------------------------------------------------------------
+/**
+ * The resource class. Allows to be collected as a garbage if not use for some time or ticks
+ *
+ * @class CacheEntry
+ * @constructor
+ * @param {ResourceManager} resource manager
+ * @param {string} key, url of the resource
+ * @param {string} item - Bitmap, HTML5Audio, WebAudio - whatever you want to store in the cache
+ */
+function CacheEntry(cache, key, item) {
+    this.cache = cache;
+    this.key = key;
+    this.item = item;
+    this.cached = false;
+    this.touchTicks = 0;
+    this.touchSeconds = 0;
+    this.ttlTicks = 0;
+    this.ttlSeconds = 0;
+    this.freedByTTL = false;
+}
+
+/**
+ * frees the resource
+ */
+CacheEntry.prototype.free = function (byTTL) {
+    this.freedByTTL = byTTL || false;
+    if (this.cached) {
+        this.cached = false;
+        delete this.cache._inner[this.key];
+    }
+};
+
+/**
+ * Allocates the resource
+ * @returns {CacheEntry}
+ */
+CacheEntry.prototype.allocate = function () {
+    if (!this.cached) {
+        this.cache._inner[this.key] = this;
+        this.cached = true;
+    }
+    this.touch();
+    return this;
+};
+
+/**
+ * Sets the time to live
+ * @param {number} ticks TTL in ticks, 0 if not set
+ * @param {number} time TTL in seconds, 0 if not set
+ * @returns {CacheEntry}
+ */
+CacheEntry.prototype.setTimeToLive = function (ticks, seconds) {
+    this.ttlTicks = ticks || 0;
+    this.ttlSeconds = seconds || 0;
+    return this;
+};
+
+CacheEntry.prototype.isStillAlive = function () {
+    var cache = this.cache;
+    return ((this.ttlTicks == 0) || (this.touchTicks + this.ttlTicks < cache.updateTicks )) &&
+        ((this.ttlSeconds == 0) || (this.touchSeconds + this.ttlSeconds < cache.updateSeconds ));
+};
+
+/**
+ * makes sure that resource wont freed by Time To Live
+ * if resource was already freed by TTL, put it in cache again
+ */
+CacheEntry.prototype.touch = function () {
+    var cache = this.cache;
+    if (this.cached) {
+        this.touchTicks = cache.updateTicks;
+        this.touchSeconds = cache.updateSeconds;
+    } else if (this.freedByTTL) {
+        this.freedByTTL = false;
+        //TODO: shall we log this event? its not normal
+        if (!cache._inner[this.key]) {
+            cache._inner[this.key] = this;
+        }
+    }
+};
+
+/**
+ * Cache for images, audio, or any other kind of resource
+ * @param manager
+ * @constructor
+ */
+function CacheMap(manager) {
+    this.manager = manager;
+    this._inner = {};
+    this._lastRemovedEntries = {};
+    this.updateTicks = 0;
+    this.lastCheckTTL = 0;
+    this.delayCheckTTL = 100.0;
+    this.updateSeconds = Date.now();
+}
+
+/**
+ * checks ttl of all elements and removes dead ones
+ */
+CacheMap.prototype.checkTTL = function () {
+    var cache = this._inner;
+    var temp = this._lastRemovedEntries;
+    if (!temp) {
+        temp = [];
+        this._lastRemovedEntries = temp;
+    }
+    for (var key in cache) {
+        var entry = cache[key];
+        if (!entry.isStillAlive()) {
+            temp.push(entry);
+        }
+    }
+    for (var i = 0; i < temp.length; i++) {
+        temp[i].free(true);
+    }
+    temp.length = 0;
+};
+
+/**
+ * cache item
+ * @param key url of cache element
+ * @returns {*|null}
+ */
+CacheMap.prototype.getItem = function (key) {
+    var entry = this._inner[key];
+    if (entry) {
+        return entry.item;
+    }
+    return null;
+};
+
+CacheMap.prototype.clear = function () {
+    var keys = Object.keys(this._inner);
+    for (var i = 0; i < keys.length; i++) {
+        this._inner[keys[i]].free();
+    }
+};
+
+CacheMap.prototype.setItem = function (key, item) {
+    return new CacheEntry(this, key, item).allocate();
+};
+
+CacheMap.prototype.update = function(ticks, delta) {
+    this.updateTicks += ticks;
+    this.updateSeconds += delta;
+    if (this.updateSeconds >= this.delayCheckTTL + this.lastCheckTTL) {
+        this.lastCheckTTL = this.updateSeconds;
+        this.checkTTL();
+    }
+};
